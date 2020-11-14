68376cc055709a4276be070c611e3d935e64a18c
Aron Weiner <aronzvi@gmail.com>
2020-11-14 19:24:27 +0700

    finished!

--- kickstart/pages/campaigns/show.js
+++ kickstart/pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Card, Grid } from 'semantic-ui-react';
+import { Card, Grid, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
 import ContributeForm from '../../components/ContributeForm';
+import { Link } from '../../routes';
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -14,6 +15,7 @@ class CampaignShow extends Component {
     console.log(summary);
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -57,7 +59,7 @@ class CampaignShow extends Component {
       {
         header: web3.utils.fromWei(balance, 'ether'),
         meta: 'Campaign Balance (ether)',
-        description: 'The balance is how much miney this campaign has left to spend.'
+        description: 'The balance is how much money this campaign has left to spend.'
       }
     ];
 
@@ -69,13 +71,25 @@ class CampaignShow extends Component {
       <Layout>
         <h3>Campaign Show</h3>
         <Grid>
-          <Grid.Column width={10}>
-            {this.renderCards()}
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              {this.renderCards()}
+            </Grid.Column>
 
-          <Grid.Column width={6}>
-            <ContributeForm />
-          </Grid.Column>
+            <Grid.Column width={6}>
+              <ContributeForm address={this.props.address}/>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Grid.Column>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>
+                      <Button primary>View Requests</Button>
+                    </a>
+                </Link>
+              </Grid.Column>
+          </Grid.Row>
         </Grid>      
       </Layout>
     );
